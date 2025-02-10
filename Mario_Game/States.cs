07884a710804ad4f0a63fd512c7ac32d9c347193
alannae <13edmondsa@student.smchull.org>
2025-02-10 11:31:30 +0000

    push

---
+++ Mario_Game/States.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario_Game
+{
+    public class States
+    {
+        static void GameState()
+        {
+
+        }
+
+        static void PauseState()
+        {
+
+        }
+
+        static void MenuState()
+        {
+
+        }
+    }
+}
