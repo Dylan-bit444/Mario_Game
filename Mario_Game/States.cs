f841ae845d43381a96754202d0d0315fd165aa88
alannae <13edmondsa@student.smchull.org>
2025-02-10 10:05:53 +0000

    finish

---
+++ Mario_Game/States.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario_Game
+{
+    public class States
+    {
+        protected ContentManager content;
+
+        protected GraphicsDeviceManager graphicsDevice;
+
+        protected Game1 Game;
+        public States(Game1 _game,GraphicsDeviceManager _graphicsDevice,ContentManager _content)
+        {
+            Game = _game;
+            graphicsDevice = _graphicsDevice;
+            content = _content;
+        }
+        static void GameState()
+        {
+            
+
+        }
+
+        static void PauseState()
+        {
+
+        }
+
+        static void MenuState()
+        {
+
+        }
+    }
+}
