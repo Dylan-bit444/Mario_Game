07d1fc52c4b0e192c396400304fcb9abc60a860f
Simon Plaskiewicz <13PlaskiewiczS@student.smchull.org>
2025-02-11 11:02:06 +0000

    Final Commit

--- Mario_Game/Game1.cs
+++ Mario_Game/Game1.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using SharpDX.Direct2D1;
 using System.Collections.Generic;
 using System.IO;
 
