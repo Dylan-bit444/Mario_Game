a60b36a3c92125ac72579204fb91c890b5901382
Dylan Wright <13WrightD@student.smchull.org>
2025-02-14 14:34:27 +0000

    Just edited the tiles

--- Mario_Game/TileManager.cs
+++ Mario_Game/TileManager.cs
@@ -1,7 +1,12 @@
 ﻿
+using System;
 using System.IO;
-using Microsoft.Xna.Framework.Graphics;
+using System.Reflection.Metadata.Ecma335;
+using Mario_Game;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 
 namespace Mario_Game
@@ -59,7 +64,7 @@ namespace Mario_Game
             return tileArray;
         }
 
-        public static Tile[,] ChooseTile(int[,] tileValuesArray, Texture2D tileSheet, GraphicsDevice graphicsDev)
+        public static Tile[,] ChooseTile(int[,] tileValuesArray, Texture2D tileSheet, GraphicsDeviceManager graphicsDev)
         {
             Tile[,] tileTypes = new Tile[tileValuesArray.GetLength(0), tileValuesArray.GetLength(1)];
             Rectangle sourceRectangle;
@@ -118,3 +123,18 @@ namespace Mario_Game
         }
     }
     }
+
+
+public static List<Rectangle> GetNearestBlocks(Rectangle Hitbox)
+{
+    int leftTile = (int)Math.Floor(Hitbox.Left / 80);
+    int rightTile = (int)Math.Ceiling((float)Hitbox.Right / 80) - 1;
+    int topTile = (int)Math.Floor((float)Hitbox.Top / 80);
+    int bottomTile = (int)Math.Ceiling((float)Hitbox.Bottom / 80) - 1;
+
+    leftTile = MathHelper.Clamp(leftTile, 0, Tile.GetLength(1));
+    rightTile = MathHelper.Clamp(rightTile, 0, Tile.GetLength(1));
+    topTile = MathHelper.Clamp(leftTile, 0, Tile.GetLength(1));
+    bottomTile = MathHelper.Clamp(rightTile, 0, Tile.GetLength(1));
+    return null;
+}
