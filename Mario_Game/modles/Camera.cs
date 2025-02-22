dcaa05b079fcda3f3e8f1bc279cb2dd679ddd21f
Dylan-bit444 <13Dwright@student.smchull.org>
2025-02-22 12:18:29 +0000

    bro

--- Mario_Game/modles/Camera.cs
+++ Mario_Game/modles/Camera.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +11,16 @@ namespace Mario_Game.modles
     internal class Camera:Sprite
     {
         private Camera() : base() { }
-        private Camera():base()
+        private Camera(Texture2D texture, Vector2 position, Color color, float volocity,int frameLength,int frameRows) : base(texture, position, color, volocity, frameLength, frameRows)
+        {
+
+        }
         public void Update(Hero hero)
         {
-            if()
+            if(Position.Y != hero.Position.Y)
+            {
+             
+            }
         }
     }
 }
