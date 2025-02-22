50a2fa6e007da822116e00602bd07b9b928c393a
Dylan-bit444 <13Dwright@student.smchull.org>
2025-02-22 12:08:02 +0000

    needed to do somthing somewhere else real quick egnore this push

---
+++ Mario_Game/modles/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario_Game.modles
+{
+    internal class Camera:Sprite
+    {
+        private Camera() : base() { }
+        private Camera():base()
+        public void Update(Hero hero)
+        {
+            if()
+        }
+    }
+}
